7d180d55770f2dc900c2e8394f9671d061bbd6eb
Sandesh Subedi <sandeshsubedi103@gmail.com>
2024-12-16 11:05:16 +0545

    Changes made to display the products available in the store

--- E-commerce-website/products/urls.py
+++ E-commerce-website/products/urls.py
@@ -5,7 +5,7 @@ from . import views
 
 urlpatterns = [
     path('', views.homepage, name='homepage'), 
-    path('products/',views.products, name='products'),
+    path('products/',views.product_list, name='products'),
     path('cart/',views.cart, name='cart'),
     path('detail/',views.detail, name='detao;'),
     
--- E-commerce-website/products/views.py
+++ E-commerce-website/products/views.py
@@ -1,7 +1,7 @@
 
 
 from django.shortcuts import render
-
+from .models import Product
 def homepage(request):
     return render(request, 'products/home.html')  
 def products(request):
@@ -10,3 +10,10 @@ def cart(request):
     return render(request,'products/cart.html')
 def detail(request):
     return render(request,'products/detail.html')
+
+
+
+def product_list(request):
+    products = Product.objects.all()  
+    return render(request, 'products/product.html', {'products': products})
+
